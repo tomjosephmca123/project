bee628e745f968c68b161f64bca77da926425d4f
tom <tomjoseph400@gmail.com>
2018-02-05 14:36:06 +0530

    filetype

---
+++ filetype.sh
@@ -0,0 +1,16 @@
+echo -n "Enter file name : "
+read file
+ 
+# find out if file has write permission or not
+[ -w $file ] && W="Write = yes" || W="Write = No"
+ 
+# find out if file has excute permission or not
+[ -x $file ] && X="Execute = yes" || X="Execute = No"
+ 
+# find out if file has read permission or not
+[ -r $file ] && R="Read = yes" || R="Read = No"
+ 
+echo "$file permissions"
+echo "$W"
+echo "$R"
+echo "$X"
